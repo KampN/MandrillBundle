14d4852d53be60b7eb7cb9287be393a1f1c9adba
olivier.jouberjean <olivier.jouberjean@kampn.com>
2020-03-25 11:29:12 +0100

    Remove symfony5 deprecated

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -43,7 +43,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('slot_mandrill');
-        $rootNode = $treeBuilder->root();
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
         ->children()
